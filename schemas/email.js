8233b524c0975f9f96e98606d97d865ff70d0084
PedroPiveta <ppivetabarrotti208@gmail.com>
2023-10-30 19:54:00 -0300

    added email

---
+++ schemas/email.js
@@ -0,0 +1,19 @@
+export default {
+    name: "email",
+    type: "document",
+    title: "Email",
+    fields: [
+        {
+            name: "nome",
+            type: "string",
+        },
+        {
+            name: "email",
+            type: "string",
+        },
+        {
+            name: "curso",
+            type: "string",
+        }
+    ]
+}
